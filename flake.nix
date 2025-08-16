1e5e03e83cb7c94d340636102fff8784264a874c
archie <suorcd@fastmail.fm>
2025-08-16 11:29:51 -0700

    updating flakaage

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   description = "Ansible development environment";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/32f313e49e42f715491e1ea7b306a87c16fe0388";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/48ce06dd94d7673eaf69508358f8af067856f3a3";
 
   outputs = { self, nixpkgs }: let
     pkgs = import nixpkgs {
